803ffe1bae80fdaae3d07262ceb69368614f1929
RDSTREEM <nahomabebe2002@gmail.com>
2026-03-01 19:03:08 +0000

    Added Rotation Transform Property

--- engine/components/transform.py
+++ engine/components/transform.py
@@ -1,6 +1,12 @@
 import numpy as np
 from engine.components.component import Component
-from engine.utils.math_utils import create_translation, create_scale
+from engine.utils.math_utils import (
+    create_translation,
+    create_scale,
+    create_rotation_x,
+    create_rotation_y,
+    create_rotation_z
+)
 
 
 class Transform(Component):
@@ -14,4 +20,11 @@ class Transform(Component):
     def get_model_matrix(self):
         translation = create_translation(self.position)
         scale = create_scale(self.scale)
-        return translation @ scale
+        
+        rx = create_rotation_x(self.rotation[0])
+        ry = create_rotation_y(self.rotation[0])
+        rz = create_rotation_z(self.rotation[0])
+        
+        rotation = rx @ ry @ rz
+
+        return translation @ rotation @ scale
