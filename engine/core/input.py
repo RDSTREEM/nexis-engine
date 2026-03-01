8f2b53dc6ef9babedfeb22c41ecfa0169e951f7b
RDSTREEM <nahomabebe2002@gmail.com>
2026-03-01 19:08:38 +0000

    Added Basic Input Hook"

---
+++ engine/core/input.py
@@ -0,0 +1,20 @@
+import pygame
+
+class Input:
+    _keys = None
+    _mouse_pos = (0,0)
+    
+    @classmethod
+    def update(cls):
+        cls._keys = pygame.key.get_pressed()
+        cls._mouse_pos = pygame.mouse.get_pos()
+
+    @classmethod
+    def get_key(cls, key):
+        if cls._keys:
+            return cls._keys[key]
+        return False
+
+    @classmethod
+    def get_mouse_position(cls):
+        return cls._mouse_pos
