6d7d8e9e5454e46485a56b1b15848101441bc05d
RDSTREEM <nahomabebe2002@gmail.com>
2026-03-02 14:43:31 +0000

    Updated mesh renderer to use mesh instead of hardcoded values

--- engine/components/mesh_renderer.py
+++ engine/components/mesh_renderer.py
@@ -1,46 +1,13 @@
-import moderngl
 import numpy as np
 from engine.components.component import Component
 
-
 class MeshRenderer(Component):
-    def __init__(self, game_object, ctx):
+    def __init__(self, game_object, mesh, material):
         super().__init__(game_object)
 
-        self.ctx = ctx
-        self.program = self.ctx.program(
-            vertex_shader="""
-                #version 330
-                uniform mat4 mvp;
-                in vec3 in_position;
-
-                void main() {
-                    gl_Position = mvp * vec4(in_position, 1.0);
-                }
-            """,
-            fragment_shader="""
-                #version 330
-                out vec4 fragColor;
-
-                void main() {
-                    fragColor = vec4(0.2, 0.6, 1.0, 1.0);
-                }
-            """
-        )
-
-        vertices = np.array([
-            -0.6, -0.4, 0.0,
-             0.6, -0.4, 0.0,
-             0.0,  0.6, 0.0,
-        ], dtype="f4")
-
-        self.vbo = self.ctx.buffer(vertices.tobytes())
-        self.vao = self.ctx.simple_vertex_array(
-            self.program,
-            self.vbo,
-            "in_position"
-        )
+        self.mesh = mesh
+        self.material = material
 
     def render(self, mvp):
-        self.program["mvp"].write(mvp.astype("f4").tobytes())
-        self.vao.render()
+        self.material.shader.set_uniform_matrix("mvp", mvp)
+        self.mesh.render()
