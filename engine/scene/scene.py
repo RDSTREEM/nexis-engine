d84a4b7dbc48989bad18c5de0e11594972a11a53
RDSTREEM <nahomabebe2002@gmail.com>
2026-03-03 09:03:08 +0000

    Added Scene Serialization for scene.py

--- engine/scene/scene.py
+++ engine/scene/scene.py
@@ -1,3 +1,6 @@
+import json
+from engine.scene.game_object import GameObject
+
 class Scene:
     def __init__(self, name="New Scene"):
         self.name = name
@@ -9,3 +12,21 @@ class Scene:
     def update(self):
         for obj in self.game_objects:
             obj.update()
+    def save(self, path):
+        data = {
+            "game_objects": [obj.to_dict() for obj in self.game_objects]
+        }
+
+        with open(path, "w") as f:
+            json.dump(data, f, indent=4)
+
+
+    def load(self, path):
+        with open(path, "r") as f:
+            data = json.load(f)
+
+        self.game_objects.clear()
+
+        for obj_data in data["game_objects"]:
+            obj = GameObject.from_dict(obj_data)
+            self.add_game_object(obj)
